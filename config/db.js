f551450d0246f10a11d266618c0fd0d6c1c1923f
Frida Kimosso <fridakimosso@gmail.com>
2026-05-13 16:52:57 +0300

    update database file

--- config/db.js
+++ config/db.js
@@ -30,7 +30,7 @@ if (process.env.DATABASE_URL) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT || 3306),
+    port: process.env.DB_PORT,
 
     waitForConnections: true,
     connectionLimit: 10,
